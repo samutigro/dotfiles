634a7c398595ec6aa8542465d08ce290900690a7
Samuele Auteri <104078093+samutigro@users.noreply.github.com>
2026-05-10 22:24:00 +0900

    wezterm

---
+++ .wezterm.lua
@@ -0,0 +1,38 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+config.colors = {
+        foreground = "#CBE0F0",
+        background = "#011423",
+        cursor_bg = "#47FF9C",
+        cursor_border = "#47FF9C",
+        cursor_fg = "#011423",
+        selection_bg = "#033259",
+        selection_fg = "#CBE0F0",
+        ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
+        brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
+}
+
+config.font = wezterm.font({
+        family = "JetBrainsMono Nerd Font",
+})
+
+config.font_size = 19
+
+config.enable_tab_bar = false
+
+config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.75
+config.macos_window_background_blur = 10
+config.audible_bell = "Disabled"
+
+-- to be able to write ~ with option+5 pressing both left and right option
+config.send_composed_key_when_left_alt_is_pressed = true
+config.send_composed_key_when_right_alt_is_pressed = true
+
+
+
+return config
